eec1aa203e50b0419e9705d698945058e1c69640
Gabriel Berti <gabriel.berti@linainfratech.com.br>
2022-11-17 15:41:46 -0300

    Aula 02. Asserções e Jest - Nessa aula, você aprendeu como: Utilizar métodos nativos de asserções para fins de comparação de igualdade; Instalar e executar o Jest com a flag --experimental; Criar arquivos de testes com o Jest e analisar erros; Usar a função describe um método do Jest usado para conjunto de testes relacionados. O describe possui a sintaxe de dois argumentos: Uma string para descrever; Uma função callback para executar o teste. Gerar relatório com o coverage, uma ferramenta integrada do jest para cobertura de testes, que possibilita identificar caminhos não testados no código.

--- index.js
+++ index.js
@@ -2,13 +2,4 @@ const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtra
 
 const calculaDesconto = (salario, desconto) => salario - desconto;
 
-const teste = (titulo, esperado, retornado) => {
-  if (esperado === retornado) {
-    return console.log(`${titulo} passou no teste.`);
-  }
-  return console.log(`${titulo} não passou no teste.`);
-};
-
-teste('somaHorasExtras', 500, somaHorasExtras(250, 300));
-
-teste('calculaDesconto', 0, calculaDesconto(250, 250));
+export { somaHorasExtras, calculaDesconto };
