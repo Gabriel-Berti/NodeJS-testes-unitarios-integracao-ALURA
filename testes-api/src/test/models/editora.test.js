9d4483028f8497ca4140917d7e6e76487c7b0d7c
Gabriel Berti <gabriel.berti@linainfratech.com.br>
2022-11-17 21:23:35 -0300

    Aula 04. Testando uma API - Nessa aula, você aprendeu como: Implementar os primeiros testes unitários em uma API; Utilizar o método skip, que serve para pular temporariamente os testes que constam na coleção de asserções; Rodar testes de forma assíncrona (com o then e await); Criar funções de simulação (mocks), com o jest. fn(), para simular um novo comportamento no teste.

---
+++ testes-api/src/test/models/editora.test.js
@@ -0,0 +1,61 @@
+import { describe, expect, it, jest } from '@jest/globals';
+import Editora from '../../models/editora';
+
+describe('Testando o modelo editora', () => {
+  const objetoEditora = {
+    nome: 'CDC',
+    cidade: 'São Paulo',
+    email: 'cdc@email.com',
+  };
+
+  it('Deve instanciar uma nova editora', () => {
+    const editora = new Editora(objetoEditora);
+    expect(editora).toEqual(expect.objectContaining(objetoEditora));
+  });
+
+  it.skip('Deve salvar editora no BD', () => {
+    const editora = new Editora(objetoEditora);
+
+    editora.salvar().then((dados) => {
+      expect(dados.nome).toBe('CDC');
+    });
+  });
+
+  it.skip('Deve salvar no DB usando a sintaxe moderna', async () => {
+    const editora = new Editora(objetoEditora);
+    const dados = await editora.salvar();
+    const valorRetornado = await Editora.pegarPeloId(dados.id);
+    expect(valorRetornado).toEqual(
+      expect.objectContaining({
+        id: expect.any(Number),
+        ...objetoEditora,
+        created_at: expect.any(String),
+        updated_at: expect.any(String),
+      })
+    );
+  });
+
+  it('Deve fazer uma chamada simulada ao banco de dados', () => {
+    const editora = new Editora(objetoEditora);
+
+    editora.salvar = jest.fn().mockReturnValue({
+      id: 10,
+      nome: 'CDC',
+      cidade: 'São Paulo',
+      email: 'cdc@email.com',
+      created_at: '2022-10-01',
+      updated_at: '2022-10-01',
+    });
+
+    const retorno = editora.salvar();
+
+    expect(retorno).toEqual(
+      expect.objectContaining({
+        id: expect.any(Number),
+        ...objetoEditora,
+        created_at: expect.any(String),
+        updated_at: expect.any(String),
+      })
+    );
+  });
+});
